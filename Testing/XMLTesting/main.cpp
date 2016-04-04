13c10a0c6c51ab5b1a6ae61a3aed7ebd7c6bdb66
LithiaH <lithiah@users.noreply.github.com>
2016-04-04 12:30:57 -0400

    start work on clustering class

--- Testing/XMLTesting/main.cpp
+++ Testing/XMLTesting/main.cpp
@@ -12,152 +12,22 @@
 #include <vector>
 #include <map>
 
-// *************** ACTOR XML STUFF ************** //
-//object to hold actor info in
-class Actor {
-public:
-    std::string name;
-    std::string gender;
-    std::string race;
-    int age;
-    int firstYear;
-    int yearRank[85] = {0}; //array of years where index in year starting 0 = 1924, and value is rank during that year, 0 if not ranked at all {issue with 0 being closer to 1 than 10}
-    
-    Actor(std::string Name, std::string Gender, std::string Race, int Age, int firstYear); //constructor
-};
-
-//constructor
-Actor::Actor(std::string Name, std::string Gender, std::string Race, int Age, int FirstYear)
-{
-    name = Name;
-    gender = Gender;
-    race = Race;
-    age = Age;
-    firstYear = FirstYear;
-}
-
-//print out each actor and corresponding key
-void printActors(std::map<int, Actor*>* actorsMap) {
-    typedef std::map<int, Actor*>::iterator it_type;
-    for(it_type iterator = actorsMap->begin(); iterator != actorsMap->end(); iterator++) {
-        std::cout << iterator->first << ": " << iterator->second->name << std::endl;
-    }
-}
-
-//get actor details and key, map together
-int mapActorsToKey(std::map<int, Actor*>* actorsMap) {
-    //doc tree of actors XML & exit if it doesn't load
+// ***************** LOAD AND READ DATA ***************** //
+void loadVectors(std::map<int, std::vector<float>>* vectorsMap, std::string fileName, int numberOfAttributes) {
     pugi::xml_document doc;
-    if (!doc.load_file("MovieStars1927to2013.xml")) return 0;
-    pugi::xml_node root = doc.child("Root");
-    //doc tree of keys
-    pugi::xml_document doc2;
-    if (!doc2.load_file("numatt.xml")) return 0;
-    pugi::xpath_node keySearch; //xpath object to find key
-    std::string keyQuery; //query for xpath containing actor name we want
-    int keyVal; //value of key associated with that actor
-    
-    //reuse vars for creating actor loop
-    std::string name;
-    std::string gender;
-    std::string race;
-    int age;
-    int firstYear;
-    
-    for (pugi::xml_node row = root.first_child(); row; row = row.next_sibling()) {
-        //create each actor
-        for (pugi::xml_node star = row.first_child().first_child(); star; star = star.next_sibling()) {
-            //pull values out from doc1 XML tree
-            name = star.first_child().first_child().value();
-            
-            gender = star.first_child().next_sibling().first_child().value();
-            race = star.first_child().next_sibling().next_sibling().first_child().value();
-            age = atoi(star.first_child().next_sibling().next_sibling().next_sibling().first_child().value());
-            firstYear = atoi(row.first_child().attribute("title").value());
-            //push new actor to actors vector
-            
-            //generate keySearch string
-            keyQuery = "//TITLE[NAME='";
-            keyQuery.append(name); //name of actor
-            keyQuery.append("']");
-    
-            //get key for actor name and turn to int
-            keySearch = doc2.select_node(&keyQuery[0]); //finds <NAME>actor</NAME> in doc2
-            keyVal = atoi(keySearch.node().first_child().next_sibling().first_child().value()); //gets key
-            
-            actorsMap->insert(std::pair<int,Actor*>(keyVal, new Actor(name, gender, race, age, firstYear)));
-        }
-    }
-    return 1;
-}
-
-//load years Actor is ranked into actor objects
-int loadYears(std::map<int, Actor*>* actorsMap) {
-    pugi::xml_document doc;
-    if (!doc.load_file("masterkey.xml")) return 0;
-    pugi::xml_node root = doc.child("Root");
-    
-    pugi::xml_node rank;
-    int actorKey;
-    int year = 0;
-    
-    for (pugi::xml_node title = root.first_child(); title; title = title.next_sibling()) {
-        rank = title.first_child().next_sibling();
-        
-        for (int i = 1; i < 11; i++) {
-            actorKey = atoi(rank.first_child().value());
-            (*actorsMap)[actorKey]->yearRank[year] = i;
-            
-            rank = rank.next_sibling();
-        }
-        year = year + 1;
-    }
-    
-    
-    return 1;
-}
-
-void constructVectors(std::map<int, Actor*>* actorsMap, std::map<int, std::vector<int> >* vectorsMap) {
-    typedef std::map<int, Actor*>::iterator it_type;
-    
-//    vars for looping
-//    int actorKey;
-    
-    for(it_type iterator = actorsMap->begin(); iterator != actorsMap->end(); iterator++) {
-        std::cout << iterator->first << ": " << iterator->second->name << std::endl;
-        vectorsMap->insert(std::pair<int, std::vector<int>>(iterator->first, *new std::vector<int>));
-        //add
-        
-        (*vectorsMap)[iterator->first].push_back(iterator->second->age);
-        (*vectorsMap)[iterator->first].push_back(iterator->second->firstYear);
-        
-        for (int i=0; i<86; i++) {
-            (*vectorsMap)[iterator->first].push_back(iterator->second->yearRank[i]);
-        }
-
-        
-    }
-}
-
-// *************** END ACTOR XML STUFF ************** //
-
-// *************** GENERALIZED XML LOADING ************** //
-void loadVectors(std::map<int, std::vector<int>>* vectorsMap, int numberOfAttributes) {
-    pugi::xml_document doc;
-    doc.load_file("masterkey.xml");
-    pugi::xml_node root = doc.child("Root");
+    doc.load_file(&fileName[0]);
+    pugi::xml_node root = doc.first_child();
     
     int j = 0;
     pugi::xml_node attr;
-    std::vector<int>* currentVector;
+    std::vector<float>* currentVector;
     
     for (pugi::xml_node title = root.first_child(); title; title = title.next_sibling()) {
         
         attr = title.first_child();
-        currentVector = new std::vector<int>;
+        currentVector = new std::vector<float>;
         
         for (int i=0; i< numberOfAttributes; i++) {
-            
             
             currentVector->push_back(atoi(attr.first_child().value()));
             
@@ -169,8 +39,8 @@ void loadVectors(std::map<int, std::vector<int>>* vectorsMap, int numberOfAttrib
     }
 }
 
-void printVectors(std::map<int, std::vector<int>>* vectorsMap, int numberOfAttributes) {
-    typedef std::map<int, std::vector<int>>::iterator it_type;
+void printVectors(std::map<int, std::vector<float>>* vectorsMap, int numberOfAttributes) {
+    typedef std::map<int, std::vector<float>>::iterator it_type;
     for(it_type iterator = vectorsMap->begin(); iterator != vectorsMap->end(); iterator++) {
         std::cout << iterator->first << ": ";
         for (int i=0; i<numberOfAttributes; i++) {
@@ -180,27 +50,69 @@ void printVectors(std::map<int, std::vector<int>>* vectorsMap, int numberOfAttri
     }
 }
 
-int main() {
-//    std::map <int, Actor*> actorsMap;
-//    std::map <int, std::vector<int>> vectorsMap;
-//    
-//    //load up data from XML
-//    if (!(mapActorsToKey(&actorsMap))) return -1; //load actors and keys into map, exit if XML loading errors
-////    printActors(&actorsMap); //print out key:actor pairs
-//    if (!(loadYears(&actorsMap))) return -1; //load year rankings into actor objects, exit if XML loading errors
-//    
-//    //generate vectors for clusters
-//    constructVectors(&actorsMap, &vectorsMap);
-////    printVectors(&vectorsMap);
-//    
-//    std::cout << (vectorsMap[4])[0];
+void populateActiveDimensions(std::vector<int>* activeDimensions, int numberOfAttributes) {
+    (*activeDimensions).push_back(0);
+    for (int i = 0; i < numberOfAttributes; i++) {
+        (*activeDimensions).push_back(1);
+    }
+}
 
-    std::map<int, std::vector<int>> vectorsMap;
-    int numberOfAttributes = 11;
-    std::string name;
+// ***************** CLUSTERING ***************** //
+class Clusters {
+public:
+    std::map<int, std::vector<float>> data;
     
-    loadVectors(&vectorsMap, numberOfAttributes);
+    int numberOfClusters; //how many clusters to use, should match centroids.size()
+    std::vector<int> centroids; //vector containing id's of points to use as cluster centroid
+    std::vector<int> means; //vector in same order as centroids containing their means
+    
+    Clusters(std::map<int, std::vector<float>> Data, int NumberOfClusters); //constructor
+    float generateMean(std::vector<float> point, std::vector<int> activeDimensions);
+    float sumOfSquares(std::vector<float> point, float mean, std::vector<int> activeDimensions);
+};
+
+Clusters::Clusters(std::map<int, std::vector<float>> Data, int NumberOfClusters) {
+    data = Data;
+    numberOfClusters = NumberOfClusters;
+}
+
+float Clusters::generateMean(std::vector<float> point, std::vector<int> activeDimensions) {
+    float mean = 0;
+    float n = 0;
+    for (int i = 0; i < point.size(); i++) {
+        if (activeDimensions[i]) {
+            mean = mean + point[i];
+            n = n + 1;
+        }
+    }
+    mean = mean / n;
+    return mean;
+}
+
+float Clusters::sumOfSquares(std::vector<float> point, float mean, std::vector<int> activeDimensions) {
+    float sumOfSquares = 0;
+    for (int i = 0; i < point.size(); i++) {
+        if (activeDimensions[i]) {
+            sumOfSquares = sumOfSquares + (sumOfSquares + (point[i] - mean)) * (sumOfSquares + (point[i] - mean));
+        }
+    }
+    return sumOfSquares;
+}
+
+
+// ************ MAIN ************* //
+int main() {
+    std::map<int, std::vector<float>> vectorsMap;
+    int numberOfAttributes = 8;
+    std::string fileName = "actorsData.xml";
+    std::vector<int> activeDimensions;
+    
+    loadVectors(&vectorsMap, fileName, numberOfAttributes);
     printVectors(&vectorsMap, numberOfAttributes);
+    populateActiveDimensions(&activeDimensions, numberOfAttributes);
     
     return 0;
 }
+
+
+
