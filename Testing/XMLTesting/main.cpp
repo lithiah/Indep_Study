db1ad470bb7d5ca551845d9a1e7d7c16c326df8d
LithiaH <lithiah@users.noreply.github.com>
2016-04-11 21:58:05 -0400

    testing on cluster method

--- Testing/XMLTesting/main.cpp
+++ Testing/XMLTesting/main.cpp
@@ -61,31 +61,54 @@ void populateActiveDimensions(std::vector<int>* activeDimensions, int numberOfAt
 // ***************** CLUSTERING ***************** //
 class Clusters {
 public:
+    //VARIABLES
+    
+    //the raw vector data from XML
     std::map<int, std::vector<float>> data;
     
-    std::vector<int> activeDimensions; //which dimensions (ie attributes) to include in calculations
+    //which dimensions to include in calculations
+    std::vector<int> activeDimensions;
     
-    std::map<int, int> clusterAssignments; //each point by mapped id assigned to cluster
-    int numberOfClusters; //how many clusters to use, should match centroids.size()
-    std::vector<std::vector<float>> centroids; //vectors of each cluster centroid (list of vectors)
+    //each point id mapped to it's cluster
+    std::map<int, int> clusterAssignments;
     
-    Clusters(std::map<int, std::vector<float>> Data, int NumberOfClusters); //constructor
-    float generateCentroid(std::map<int, std::vector<float>> data, std::vector<int> activeDimensions);
-    float distance(std::vector<float> point, std::vector<float> centroid, std::vector<int> activeDimensions);
-    void setupClusters(std::map<int, std::vector<float>> data, std::vector<std::vector<float>> centroids, int numberOfClusters);
-    void iterateAssignmentStep(std::map<int, std::vector<float>> data, std::map<int, int> clusterAssignments, std::vector<std::vector<float>> centroids, std::vector<int> activeDimensions);
+    //vector of each cluster's size
+    std::map<int, int> clusterSizes;
+    
+    //how many clusters to use
+    int numberOfClusters;
+    
+    //vectors of each cluster centroid
+    std::vector<std::vector<float>> centroids;
+    
+    //SETUP
+    //constructor
+    Clusters(std::map<int, std::vector<float>> Data, int NumberOfClusters, std::vector<int> activeDimensions);
+    
+    //initialize cluster centroids
+    void setupClusters();
+    
+    //ALGORITHM
+    //complete one iteration of assignment
+    void iterateAssignmentStep();
+    
+    //RESULTS
+    void printClusterAssignments();
+    
+private:
+    //HELPER FUNCTIONS
+    //calculate distance between a point and centroid
+    float distance(std::vector<float> point, std::vector<float> centroid);
+    
 };
 
-Clusters::Clusters(std::map<int, std::vector<float>> Data, int NumberOfClusters) {
+Clusters::Clusters(std::map<int, std::vector<float>> Data, int NumberOfClusters, std::vector<int> ActiveDimensions) {
     data = Data;
     numberOfClusters = NumberOfClusters;
+    activeDimensions = ActiveDimensions;
 }
 
-float Clusters::generateCentroid(std::map<int, std::vector<float>> data, std::vector<int> activeDimensions) {
-    return 2.3;
-}
-
-float Clusters::distance(std::vector<float> point, std::vector<float> centroid, std::vector<int> activeDimensions) {
+float Clusters::distance(std::vector<float> point, std::vector<float> centroid) {
     float distance = 0;
     for (int i = 0; i < point.size(); i++) {
         if (activeDimensions[i]) {
@@ -95,7 +118,7 @@ float Clusters::distance(std::vector<float> point, std::vector<float> centroid, 
     return sqrt(distance);
 }
 
-void Clusters::setupClusters(std::map<int, std::vector<float>> data, std::vector<std::vector<float>> centroids, int numberOfClusters) {
+void Clusters::setupClusters() {
     //assign inital clusters based on first n data points [!! WILL CHANGE THIS]
     
     for (int i=0; i<numberOfClusters; i++) {
@@ -103,35 +126,60 @@ void Clusters::setupClusters(std::map<int, std::vector<float>> data, std::vector
     }
 }
 
-void Clusters::iterateAssignmentStep(std::map<int, std::vector<float>> data, std::map<int, int> clusterAssignments, std::vector<std::vector<float>> centroids, std::vector<int> activeDimensions) {
+void Clusters::iterateAssignmentStep() {
     //1: for each point calculate distance from centroid, determine which distance is smallest, assign to cluster with smaller distance
     int closestCluster;
-    float minDistance = 1000000000000;
+    float minDistance;
     float newDistance;
     //for each point
     for (int i=0; i<data.size(); i++) {
         closestCluster = 0;
+        minDistance = 1000000000;
         //for each cluster
         for (int j=0; j<centroids.size(); j++) {
-            newDistance = distance(data[i], centroids[i], activeDimensions);
+            
+            std::cout << "data[i] (x,y): " << data[i][0] << ", " << data[i][1] << std::endl;
+            std::cout << "centroids[j] (x,y): " << centroids[j][0] << ", " << centroids[j][1] << std::endl;
+            
+            newDistance = distance(data[i], centroids[j]);
+            
+            std::cout << "newDistance: " << newDistance << std::endl;
+            std::cout << "minDistance: " << minDistance << std::endl;
+            
             if (newDistance < minDistance) {
+                std::cout << "Reassigning to newDistance" << std::endl;
                 closestCluster = j;
                 minDistance = newDistance;
             }
+
+            std::cout << std::endl << "---------------------" << std::endl << std::endl;
         }
+        
+        clusterAssignments[i] = closestCluster;
+        
     }//assigned cluster to each point
-    
-    float n = 0;
     
     //2: calculate new centroid for each cluster
     //for each point add dimension values to centroid
+    
     //first clear centroids
+    for (int i=0; i<clusterSizes.size(); i++) {
+        clusterSizes[i] = 0;
+    }
+    for (int i=0; i<centroids.size(); i++) {
+        for (int j=0; j<centroids[0].size(); j++) {
+            centroids[i][j] = 0;
+        }
+    }
+
     
     //add each point to corresponding centroid
     for (int i=0; i<data.size(); i++) {
+        clusterSizes[clusterAssignments[i]] = clusterSizes[clusterAssignments[i]] + 1;
+        
         //for each dimension
-        n = n + 1;
         for (int j=0; j<data[i].size(); j++){
+            
             if (activeDimensions[j]) {
                 centroids[clusterAssignments[i]][j] = centroids[clusterAssignments[i]][j] + data[i][j];
             }
@@ -139,8 +187,22 @@ void Clusters::iterateAssignmentStep(std::map<int, std::vector<float>> data, std
     }
     for (int i=0; i<centroids.size(); i++) {
         for (int j=0; j<centroids[0].size(); j++) {
-            centroids[i][j] = centroids[i][j] / n;
+            if (clusterSizes[i] > 0) {
+                centroids[i][j] = centroids[i][j] / clusterSizes[i];
+            }
+            else {
+                centroids[i][j] = 0;
+            }
         }
+    }
+}
+
+void Clusters::printClusterAssignments() {
+    typedef std::map<int, int>::iterator it_type;
+    
+    for(it_type iterator = clusterAssignments.begin(); iterator != clusterAssignments.end(); iterator++) {
+        std::cout << iterator->first << ": ";
+        std::cout << iterator->second << std::endl;
     }
 }
 
@@ -148,13 +210,25 @@ void Clusters::iterateAssignmentStep(std::map<int, std::vector<float>> data, std
 // ************ MAIN ************* //
 int main() {
     std::map<int, std::vector<float>> vectorsMap;
-    int numberOfAttributes = 8;
-    std::string fileName = "actorsData.xml";
+    int numberOfAttributes = 3;
+    std::string fileName = "testFile.xml";
     std::vector<int> activeDimensions;
     
+    activeDimensions.push_back(0);
+    activeDimensions.push_back(1);
+    activeDimensions.push_back(0);
+    
     loadVectors(&vectorsMap, fileName, numberOfAttributes);
-    printVectors(&vectorsMap, numberOfAttributes);
+//    printVectors(&vectorsMap, numberOfAttributes);
     populateActiveDimensions(&activeDimensions, numberOfAttributes);
+    
+    Clusters testCase = Clusters(/*data*/ vectorsMap, /*number of clusters*/ 2, /*which dims to use */ activeDimensions);
+    
+    testCase.setupClusters();
+    testCase.iterateAssignmentStep();
+    testCase.printClusterAssignments();
+    testCase.iterateAssignmentStep();
+    testCase.printClusterAssignments();
     
     return 0;
 }
