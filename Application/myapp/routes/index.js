6f91d1c4ac0147f522fa6eb19c1ad4975a28388f
LithiaH <lithiah@users.noreply.github.com>
2016-04-23 18:52:08 -0400

    update modules

--- Application/myapp/routes/index.js
+++ Application/myapp/routes/index.js
@@ -3,20 +3,10 @@ var cpphello = require('cpphello');
 
 var router = express.Router();
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Testing'});
-// });
-
 router.get('/', function(req, res, next) {
-    var number = parseInt(req.body.number);
-    console.log("input number: ", number);
 
-    res.render('index', { title: 'Testing', sentValue: number, cppOutput: cpphello.timesTwo(number), threadTest: cpphello.threadTest(), threadSum: cpphello.threadSum(number) });
+    res.render('index', { title: 'Testing', clusterResults: cpphello.clustersInit("testfile.xml", 10, 2, 2, [1, 1])});
 
-    var clustersInit = cpphello.clustersInit("testfile.xml", 10, 2, 2, [1,1]);
-
-    console.log(clustersInit);
 })
 
 module.exports = router;
