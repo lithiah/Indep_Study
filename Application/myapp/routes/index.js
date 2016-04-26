b97ed106d42f90ab8615f9c987319a523ddcae61
LithiaH <lithiah@users.noreply.github.com>
2016-04-26 17:48:48 -0400

    FINALLY integrated clusters add-on to app

--- Application/myapp/routes/index.js
+++ Application/myapp/routes/index.js
@@ -5,7 +5,17 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
 
-    res.render('index', { title: 'Testing', clusterResults: cpphello.clustersInit("testfile.xml", 10, 2, 2, [1, 1])});
+    clusters = cpphello.clustersInit(/*all data points data as 1 vector*/[1, 1, 2, 1, 4, 3, 5, 4], /*number of iterations*/10, /*number of clusters*/2, /*number of attributes*/2, /*active dimensions*/[1, 1]);
+
+    console.log("Cluster Assignment Results: ", clusters);
+    console.log(typeof(clusters));
+    var i = 0;
+    for (thing in clusters) {
+        console.log(i, ": ", clusters[i]);
+        i++;
+    }
+
+    res.render('index', { title: 'Testing', clusterResults: clusters});
 
 })
 
